d0279cd7ce7ab5bf4abad2ab0799513df33826ab
bakala12 <bakala12@o2.pl>
2016-03-09 22:58:11 +0100

    Message base abstract class

---
+++ ComputationalCluster/CommunicationsUtils/Messages/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationsUtils.Messages
+{
+    public abstract class Message
+    {
+        protected Message(MessageType type)
+        {
+            Type = type;
+        }
+
+        [System.Xml.Serialization.XmlIgnore]
+        public MessageType Type { get; } 
+    }
+
+    public enum MessageType
+    {
+        DivideProblemMessage,
+        NoOperationMessage,
+        SolvePartialProblemsMessage,
+        RegisterMessage, 
+        RegisterResponseMessage, 
+        SolutionsMessage,
+        SolutionRequestMessage,
+        SolveRequestMessage,
+        SolveRequestResponseMessage,
+        StatusMessage
+    }
+}

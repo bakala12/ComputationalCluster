7cdfc6670192edf6699ab0c4ecdf7c78aa4937d6
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-03-11 19:52:07 +0100

    Create basic implementation of Cluster Server

--- ComputationalCluster/CommunicationsUtils/NetworkInterfaces/ClusterListener.cs
+++ ComputationalCluster/CommunicationsUtils/NetworkInterfaces/ClusterListener.cs
@@ -1,17 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using CommunicationsUtils.Messages;
+using CommunicationsUtils.Serialization;
 
 namespace CommunicationsUtils.NetworkInterfaces
 {
     /// <summary>
-    /// Computational Cluster Listener component.
+    /// Computational Cluster Listener component (server-side)
     /// Provides configuration, sending and retrieving messages
     /// </summary>
     public class ClusterListener
     {
-        
+        private TcpListener tcpListener;
+        private int port;
+        private string address;
+        private MessageToBytesConverter converter = new MessageToBytesConverter();
+        private Socket currentSocket = null;
+
+        public ClusterListener(string _address, int _port)
+        {
+            address = _address;
+            port = _port;
+            IPAddress ip = IPAddress.Parse(_address);
+            tcpListener = new TcpListener(ip, port);
+        }
+
+        public void StartListening ()
+        {
+            tcpListener.Start();
+        }
+
+        /// <summary>
+        /// waits for connection, then receives request(s)
+        /// </summary>
+        /// <returns>messages passed to socket</returns>
+        public Message[] WaitForRequest ()
+        {
+            currentSocket = tcpListener.AcceptSocket();
+            byte[] requestBytes = new byte[Properties.Settings.Default.MaxRequestSize];
+            currentSocket.Receive(requestBytes);
+            return converter.BytesToMessages(requestBytes);
+        }
+
+        /// <summary>
+        /// sends response(s) via open connection
+        /// </summary>
+        /// <param name="responses"></param>
+        public void SendResponse (Message[] responses)
+        {
+            if (currentSocket == null)
+            {
+                throw new Exception("Something went wrong with connection (socket)");
+            }
+            byte[][] responsesBytes = converter.MessagesToBytes(responses);
+
+            for (int i = 0; i < responsesBytes.GetLength(0); i++)
+            {
+                currentSocket.Send(responsesBytes[i]);
+                currentSocket.Send(new byte[] { 23 });
+            }
+
+            currentSocket.Close();
+            currentSocket = null;
+        }
     }
 }
--- ComputationalCluster/CommunicationsUtils/Properties/Settings.Designer.cs
+++ ComputationalCluster/CommunicationsUtils/Properties/Settings.Designer.cs
@@ -34,5 +34,17 @@ namespace CommunicationsUtils.Properties {
                 this["MaxResponseSize"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1000")]
+        public int MaxRequestSize {
+            get {
+                return ((int)(this["MaxRequestSize"]));
+            }
+            set {
+                this["MaxRequestSize"] = value;
+            }
+        }
     }
 }

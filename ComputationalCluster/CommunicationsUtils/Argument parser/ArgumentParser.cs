3f3959debf1c0a2c09f17d15dedda95f61a7e3c5
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-03-12 14:00:08 +0100

    Argument parser

---
+++ ComputationalCluster/CommunicationsUtils/Argument parser/ArgumentParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Runtime.InteropServices;
+using NDesk.Options;
+// ReSharper disable InconsistentNaming
+
+namespace CommunicationsUtils.Argument_parser
+{
+    public class ArgumentParser
+    {
+        private readonly Dictionary<string, string> map = new Dictionary<string, string>();
+        private readonly OptionSet options;
+
+        public ArgumentParser(IEnumerable<string> options)
+        {
+            this.options = new CustomOptionSet();
+            foreach (var o in options)
+            {
+                Action<string> action = s =>
+                {
+                    map.Add(o, s);
+                };
+                this.options.Add(o, action);
+            }
+        }
+
+        public void ProcessArguments(string[] args)
+        {
+            options.Parse(args);
+            var keys = new List<string>(map.Keys);
+            keys.Sort();
+            //TODO
+            foreach (var key in keys)
+            {
+                Console.WriteLine("Key: {0}={1}", key, map[key]);
+            }
+        }
+    }
+}
---
+++ ComputationalCluster/CommunicationsUtils/Argument parser/OptionSetPool.cs
@@ -0,0 +1,15 @@
+﻿namespace CommunicationsUtils.Argument_parser
+{
+    public class OptionSetPool
+    {
+        public static string[] ServerOptionsSet => new[]
+        {
+            "port=","time=","backup","maddress=","mport="
+        };
+
+        public static string[] ClientOptionsSet => new[]
+        {
+            "address=","port="
+        };
+    }
+}

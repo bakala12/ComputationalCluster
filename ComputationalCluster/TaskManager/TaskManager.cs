1b99a1a317df1fa4b94e3ca27c47ae0085d5cb79
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-03-13 00:05:30 +0100

    Make very basic classes for task manager

---
+++ ComputationalCluster/TaskManager/TaskManager.cs
@@ -0,0 +1,24 @@
+﻿using CommunicationsUtils.NetworkInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class TaskManager
+    {
+        IClusterClient clusterClient;
+
+        public TaskManager(IClusterClient _clusterClient)
+        {
+            clusterClient = _clusterClient;
+        }
+
+        public void Run ()
+        {
+
+        }
+    }
+}

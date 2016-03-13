e288751a2e1892fe6f0de5d312eee9dcb3bc97d1
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-03-13 02:06:16 +0100

    Complete code for communication of TM (except for failure cases)

---
+++ ComputationalCluster/TaskManager/ProblemInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class ProblemInfo
+    {
+        public int ProblemsCount = 0;
+        public int SolutionsCount = 0;
+        public string ProblemType = "";
+        /// <summary>
+        /// partial problems indexed by task id (given by TM)
+        /// </summary>
+        public Dictionary<ulong, PartialInfo> PartialProblems;
+        public ProblemInfo()
+        {
+            PartialProblems = new Dictionary<ulong, PartialInfo>();
+        }
+    }
+}

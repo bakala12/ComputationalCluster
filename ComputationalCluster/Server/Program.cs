adc6a108415cbd6cbc732b99ee8aaf6e3032d627
Tygrysku <duszakp@student.mini.pw.edu.pl>
2016-03-17 12:08:36 +0100

    Changing server to use its public ip adress on start.

--- ComputationalCluster/Server/Program.cs
+++ ComputationalCluster/Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using CommunicationsUtils.Argument_parser;
 using CommunicationsUtils.NetworkInterfaces.Factories;
@@ -15,8 +16,8 @@ namespace Server
             parser.ProcessArguments(args);
             parser.UpdateConfiguration(parser.map);
 
-            var address = Properties.Settings.Default.Address;
-            var port = Properties.Settings.Default.Port;
+            var address = GetPublicIp();
+            var  port = Properties.Settings.Default.Port;
             var state = Properties.Settings.Default.IsBackup ? ServerState.Backup : ServerState.Primary;
 
             IPAddress ipAddress;
@@ -24,9 +25,28 @@ namespace Server
             {
                 throw new Exception("Invalid ip address");
             }
-            var listener = ClusterListenerFactory.Factory.Create(IPAddress.Any, port);
+            var listener = ClusterListenerFactory.Factory.Create(ipAddress, port);
             var server = new ComputationalServer(listener, state); 
             server.Run();
+        }
+
+        private static string GetPublicIp()
+        {
+            string direction;
+            var request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (var response = request.GetResponse())
+                // ReSharper disable once AssignNullToNotNullAttribute
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                direction = stream.ReadToEnd();
+            }
+
+            //Search for the ip in the html
+            var first = direction.IndexOf("Address: ", StringComparison.Ordinal) + 9;
+            var last = direction.LastIndexOf("</body>", StringComparison.Ordinal);
+            direction = direction.Substring(first, last - first);
+            Console.WriteLine("Public ip of this machine is: " + direction);
+            return direction;
         }
     }
 }

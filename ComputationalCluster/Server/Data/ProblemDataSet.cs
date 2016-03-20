97b166d61372f619c125772c3287894804bfd352
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-03-20 01:51:34 +0100

    Add new field to ProblemDataSet class to boost effectivenes of some subroutines to-do

--- ComputationalCluster/Server/Data/ProblemDataSet.cs
+++ ComputationalCluster/Server/Data/ProblemDataSet.cs
@@ -20,6 +20,11 @@ namespace Server.Data
         /// </summary>
         public byte[] CommonData { get; set; }
         /// <summary>
+        /// counter of solutions acquired from CN. this will boost GetMessageFor*
+        /// subroutines' effectiveness (from linear to constant complexity)
+        /// </summary>
+        public int SolutionsCount { get; set; }
+        /// <summary>
         /// array of subproblems (and corresponding solutions)
         /// acquired from TM after sending DivideProbllem msg
         /// </summary>

fe6576f7f729961e8c6c1257a64f568406d8bb58
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-04-12 22:15:31 +0200

    Add task solver dll, DvrpTaskSolver class (not implemented)

---
+++ ComputationalCluster/AlgorithmSolvers/DVRPTaskSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCCTaskSolver;
+
+namespace AlgorithmSolvers
+{
+    /// <summary>
+    /// Task solver implementation for DVRP
+    /// </summary>
+    public class DvrpTaskSolver : TaskSolver
+    {
+        public DvrpTaskSolver(byte[] problemData) : base(problemData)
+        {
+        }
+
+        public override byte[] Solve(byte[] partialData, TimeSpan timeout)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[][] DivideProblem(int threadCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] MergeSolution(byte[][] solutions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name { get; }
+    }
+}

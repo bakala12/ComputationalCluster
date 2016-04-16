e0668dc774ba1ba1151b2da04feace08c7b13113
bondyraj <bondyraj@student.mini.pw.edu.pl>
2016-04-16 20:53:13 +0200

    Add incomplete solve implementation

--- ComputationalCluster/AlgorithmSolvers/DVRPEssentials/DVRPTaskSolver.cs
+++ ComputationalCluster/AlgorithmSolvers/DVRPEssentials/DVRPTaskSolver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +17,80 @@ namespace AlgorithmSolvers.DVRPEssentials
         {
         }
 
+        ///
+        ///
+        ///SOLVE PROBLEM
+        /// 
+        /// 
         public override byte[] Solve(byte[] partialData, TimeSpan timeout)
         {
             //przerób bajty na PartialProblemInstance
-            //w pętli (dla każdego samochodu odpal jakąś funkcję, która posortuje
-            //klientów tak by koszt był najniższy
-            //jeżeli nie można (demands są za duże lub czasy są złe)
-            //to wyjdź
+            var converter = new ProblemToBytesConverter();
+            var partialInstance = (DVRPPartialProblemInstance) converter.FromBytesArray(partialData);
+            var instance = (DVRPProblemInstance) converter.FromBytesArray(_problemData);
+            //dla każdego samochodu (listy klientów mu przypisanej) sprawdz poprawność kombinacji
+            foreach (var carVisits in partialInstance.VisitIds)
+            {
+                //sprawdź czy demands < capacity
+                if (!checkDemands(instance, carVisits, instance.VehicleCapacity))
+                {
+                    return solutionImpossible();
+                }
+
+            }
+            //po wstepnej walidacji, czas na takie ustawienie id'ków dla każdego samochodu,
+            //aby koszt był najmniejszy (jak się nie da, to zwróć -1)
+            int totalCost = 0;
+
+            for (var i = 0; i<partialInstance.VisitIds.Length; i++)
+            {
+                var currCost = minimizePermutation(instance, ref partialInstance.VisitIds[i]);
+                if (currCost == -1)
+                {
+                    return solutionImpossible();
+                }
+
+                totalCost += currCost;
+            }
+
+            partialInstance.SolutionResult = SolutionResult.Successful;
+            partialInstance.PartialResult = totalCost;
+            return converter.ToByteArray(partialInstance);
             //timeoutu nie rozpatruj, szkoda zdrowia
+        }
+
+        /// <summary>
+        /// minimalizacja kosztu
+        /// </summary>
+        /// <param name="instance">dane całego problemu</param>
+        /// <param name="carVisits">permutacje dla samochodu - do minimalizacji kosztu</param>
+        /// <returns>koszt minimalnej permutacji, -1 w przypadku permutacji niemozliwej</returns>
+        private int minimizePermutation(DVRPProblemInstance instance, ref List<int> carVisits)
+        {
+            //generacja wszystkich permutacji i sprawdzanie kosztu (zlozonosc n!)
             throw new NotImplementedException();
         }
 
+        private byte[] solutionImpossible()
+        {
+            var converter = new ProblemToBytesConverter();
+            return converter.ToByteArray(new DVRPPartialProblemInstance()
+            {
+                SolutionResult = SolutionResult.Impossible
+            });
+        }
+
+        private bool checkDemands(DVRPProblemInstance instance, List<int> carVisits, int vehicleCapacity)
+        {
+            var demands = carVisits.Sum(v => instance.Visits.Single(x => x.Id == v).Demand);
+            return Math.Abs(demands) < vehicleCapacity;
+        }
+
+        ///
+        ///
+        ///DIVIDE PROBLEM
+        /// 
+        /// 
         public override byte[][] DivideProblem(int threadCount)
         {
             var converter = new ProblemToBytesConverter();
@@ -80,7 +144,11 @@ namespace AlgorithmSolvers.DVRPEssentials
                 }
             }
         }
-
+        ///
+        ///
+        ///MERGE SOLUTION
+        /// 
+        /// 
         public override byte[] MergeSolution(byte[][] solutions)
         {
             var converter = new ProblemToBytesConverter();
